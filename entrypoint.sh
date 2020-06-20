403d2edc6d3b4117f9f7e33d6bb992e13720c913
Joseph Kato <joseph@jdkato.io>
2020-06-20 15:14:57 -0700

    WIP

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,6 @@
 #!/bin/sh
 
 echo "Running Vale ..."
-<<<<<<< HEAD
-
-matcher_path=`pwd`/vale.json
-cp /vale.json "$matcher_path"
-
-=======
->>>>>>> origin/master
 echo "::add-matcher::vale.json"
 
 node /lib/main.js
