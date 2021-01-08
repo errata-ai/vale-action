62398a3f5d0b9332ec7d0d3e96bd40bb8107c92b
Joseph Kato <joseph@jdkato.io>
2021-01-08 13:10:35 -0800

    Add basic tests
    Signed-off-by: Joseph Kato <joseph@jdkato.io>

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,4 +1,70 @@
-describe('TODO - Add a test suite', () => {
-  it('TODO - Add a test', async () => {
+import * as github from '@actions/github';
+
+import {parsePatch} from '../src/git';
+
+describe('GitHub API usage', () => {
+  const token = process.env.GITHUB_TOKEN as string;
+  const gh = github.getOctokit(token);
+
+  it('should get commits from a PR', async () => {
+    const resp = await gh.request(
+      'GET /repos/{owner}/{repo}/pulls/{pull_number}',
+      {
+        owner: 'errata-ai',
+        repo: 'vale',
+        pull_number: 279
+      }
+    );
+
+    const commits = await gh.paginate(`GET ${resp.data.commits_url}`, {
+      owner: 'errata-ai',
+      repo: 'vale'
+    });
+
+    expect(commits.length).toEqual(1);
+  });
+
+  it('should get modified lines from a commit', async () => {
+    const resp = await gh.repos.getCommit({
+      owner: 'errata-ai',
+      repo: 'vale',
+      // https://github.com/errata-ai/vale/pull/279
+      ref: 'bd7a022c9253297cc21a3dfa0bd5fc71e56dc215'
+    });
+
+    resp.data.files.forEach(file => {
+      const lines = parsePatch(file.patch);
+      expect(lines).toEqual([
+        329,
+        330,
+        331,
+        332,
+        333,
+        334,
+        335,
+        336,
+        337,
+        338,
+        339,
+        340,
+        341,
+        342,
+        343,
+        344,
+        345,
+        346,
+        347,
+        348,
+        349,
+        350,
+        351,
+        352,
+        353,
+        354,
+        355,
+        356,
+        357
+      ]);
+    });
   });
 });
--- src/git.ts
+++ src/git.ts
@@ -78,3 +78,27 @@ function ensureGitHistory() {
   execa.commandSync('git fetch origin');
   isHhistoryLoaded = true;
 }
+
+export function parsePatch(patch: string): number[] {
+  return patch.split(/(?:\r\n|\r|\n)/g)
+    // compute the lines that have been added. e.g: `[1, 42]`.
+    .reduce((acc: number[], line: string) => {
+      // lines starting with @@ mark a hunk. the format is like this:
+      // @@ -(start of removals),(number of removed lines) +(start of insertions),(number of insertions)
+      // here are some examples:
+      // @@ -33,0 +42,24 @@ => removes nothing and inserts 24 lines starting at line 42
+      // @@ -8 +6 @@        => removes line 8 and adds line 6. if there's no comma it's a single-line change.
+      if (!line.startsWith('@@')) return acc;
+
+      // get the `+` portion, as only additions are relevant in order to filter annotations for portions that are changed.
+      // afterwards split by `,` (no `,` means a single line addition).
+      const [start, numberOfAddedLines = 1] = line.split(' ')[2].split(',');
+
+      const startInt = parseInt(start, 10);
+      for (let i = 0; i < numberOfAddedLines; i++) {
+        acc.push(startInt + i);
+      }
+      return acc;
+    }, [])
+}
+
