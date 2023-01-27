b865429f569da2a3c22c42c8b1d70ebeb16cdad5
jdkato <joseph@jdkato.io>
2023-01-26 18:43:41 -0800

    WIP

--- src/input.ts
+++ src/input.ts
@@ -94,7 +94,7 @@ export async function get(tok: string, dir: string): Promise<Input> {
   }
 
   let args: string[] = [
-    ` --no-wrap`,
+    `--no-wrap`,
     //`--output=${path.resolve(__dirname, 'rdjsonl.tmpl')}`,
     ...parsedFlags,
   ];
