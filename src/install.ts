88e26afa37b6da311d9bf461a20db28c9a7511b8
Joseph Kato <joseph@jdkato.io>
2025-06-03 23:22:23 -0700

    Merge pull request #149 from StaticRocket/reviewdog
    feat: allow use of local version of vale

--- src/install.ts
+++ src/install.ts
@@ -1,4 +1,5 @@
 import * as core from '@actions/core';
+import * as exec from '@actions/exec';
 import * as tc from '@actions/tool-cache';
 import fetch from 'node-fetch';
 import path from 'path';
@@ -6,7 +7,31 @@ import path from 'path';
 const releases = 'https://github.com/errata-ai/vale/releases/download';
 const last = 'https://github.com/errata-ai/vale/releases/latest/';
 
+async function lookupLint(): Promise<string> {
+  let path = '';
+  let stderr = '';
+
+  let resp = await exec.exec('which', ['vale'], {
+    listeners: {
+      stdout: (buffer: Buffer) => (path = buffer.toString().trim()),
+      stderr: (data: Buffer) => (stderr += data.toString())
+    }
+  });
+
+  if (resp !== 0) {
+    core.setFailed(stderr);
+  }
+
+  core.info(`Using the install at ${path}`)
+  return path;
+}
+
 export async function installLint(version: string): Promise<string> {
+  if (version === 'none') {
+    core.info(`Assuming a version of vale is already available.`);
+    return await lookupLint();
+  }
+
   core.info(`Installing Vale version '${version}' ...`);
   if (version === 'latest') {
     const response = await fetch(last);
